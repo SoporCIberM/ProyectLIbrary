85135428ae2fa70c8d8f90459f9a79eb74c4afc5
root <root@Dedian.myguest.virtualbox.org>
2025-09-06 02:01:35 -0500

    CReacion del Proyecto

---
+++ backend/src/test/java/library/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package library.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
