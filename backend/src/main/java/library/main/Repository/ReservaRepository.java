6a54202ed92bddbad7d34994ae6545b8a8039913
Chris3012Mega <cristofer6666w6@gmail.com>
2025-09-17 19:50:17 -0500

    Creacion del repositorio reserva

---
+++ backend/src/main/java/library/main/Repository/ReservaRepository.java
@@ -0,0 +1,9 @@
+package library.main.Repository;
+
+import library.main.Entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservaRepository extends JpaRepository<Reserva , Long> {
+
+    
+}
